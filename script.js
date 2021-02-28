6c21ff0c2f26d9fdd3dd2c63807c4fb640cce24b
ronaldavs <79800203+ronaldavs@users.noreply.github.com>
2021-02-28 22:47:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Ronaldav" && b=="Ganteng123")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
